a2795a4ca354f3ce29c3a74154a532c13ebb56ba
Simon_996 <kantachat_2539@hotmail.com>
2025-06-15 02:44:12 -0700

    Initial Commit

---
+++ conicle-video-challenge-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  conicle-video-challenge-ios
+//
+//  Created by Simon SIwell on 15/6/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
